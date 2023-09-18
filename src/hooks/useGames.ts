f181d5351cf4ab5de38ffc567cded153727e21c0
huzai7a-dev <shaikhhuzaifa072@gmail.com>
2023-09-18 07:02:08 -0400

    built game card

---
+++ src/hooks/useGames.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
+
+
+export interface Game {
+    id: number,
+    name: string,
+    background_image: string
+}
+
+interface FecthGameResponse {
+    count: number,
+    results: Game[]
+}
+
+const useGames = () => {
+    const [games, setGames] = useState<Game[]>([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    useEffect(() => {
+        const controller = new AbortController()
+        apiClient.get<FecthGameResponse>('/games', {signal:controller.signal})
+            .then((res) => setGames(res.data.results))
+            .catch((err) => {
+                if (err instanceof CanceledError) return
+                setErrorMessage(err.message)
+            })
+
+        return ()=> controller.abort()
+    }, [])
+
+    return {games,errorMessage}
+}
+
+export default useGames;
