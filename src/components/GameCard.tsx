1afa910cb7359c790f2fcd7365fbf1a634ec32d2
huzai7a-dev <shaikhhuzaifa072@gmail.com>
2023-09-18 16:53:17 -0400

    displayed score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react"
 import { Game } from "../hooks/useGames"
 import PlatformIconList from "./PlatformIconList"
+import CriticScore from "./CriticScore"
 
 interface Props {
     game: Game
@@ -12,7 +13,7 @@ const GameCard = ({ game }: Props) => {
             <CardBody>
                 <HStack justifyContent='space-between' marginBottom={3}>
                     <PlatformIconList platforms={game.parent_platforms?.map(p => p.platform)} />
-                    {/* <CriticScore score={game.metacritic} /> */}
+                    <CriticScore score={game.metacritic} />
                 </HStack>
                 <Heading fontSize='2xl'>{game.name}
                     {/* <Emoji rating={game.rating_top} /> */}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -11,8 +11,10 @@ export interface Platform {
 export interface Game {
     id: number,
     name: string,
-    background_image: string
-    parent_platforms: {platform:Platform}[]
+    background_image: string,
+    parent_platforms: { platform: Platform }[],
+    metacritic:number
+    
 }
 
 interface FecthGameResponse {
