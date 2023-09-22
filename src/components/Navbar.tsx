8c4e1be2e1b7705c436565db2454f47080e991d3
huzai7a-dev <shaikhhuzaifa072@gmail.com>
2023-09-22 05:59:32 -0400

    created game search input

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react"
 import logo from '../assets/logo.webp';
 import SwitchDarkMode from "./SwitchDarkMode";
+import SearchInput from "./SearchInput";
 const Navbar = () => {
     return (
-        <HStack paddingY={'8px'} justifyContent={"space-between"}>
+        <HStack paddingY={'8px'}>
             <Image src={logo} boxSize="60px" />
+            <SearchInput/>
             <SwitchDarkMode/>
         </HStack>
     )
